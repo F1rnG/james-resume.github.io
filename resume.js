1d701d85d6ed84f980db4af82ef44c4fa4a4079d
jkalnderson09 <jkanderson09@gmail.com>
2017-02-12 12:54:03 -0500

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	$(".menu").hide();
+	$(".cross").hide();
+
+
+$(".hamburger").click(function(){
+		$(".menu").slideToggle("slow", function(){
+		$(".hamburger").hide();
+		$(".cross").show();
+	});
+});
+
+
+$(".cross").click(function(){
+	$(".menu").slideToggle("slow", function(){
+		$(".cross").hide();
+		$(".hamburger").show();
+	});
+});
+});
+
